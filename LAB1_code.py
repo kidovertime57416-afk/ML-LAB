6baaad106395e35590e112598739f07be28ba38a
kidovertime <kidovertime57416@gmail.com>
2026-07-19 21:43:05 +0700

    Add Data Visualization

--- LAB1_code.py
+++ LAB1_code.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 #1.Load Dataset
 df = pd.read_csv(r"C:\Users\Kidovertime\Desktop\ML\LAB01\vgsales.csv")
@@ -26,3 +28,18 @@ print(df.duplicated().sum())
 #7.Display Class Distribution
 print("\n------------------------------")
 print(df['Genre'].value_counts())
+
+# 1. Histogram
+plt.figure(figsize=(8, 5))
+sns.histplot(df['Global_Sales'], bins=50, kde=True)
+plt.title('Histogram of Global Sales')
+plt.xlabel('Global Sales')
+plt.ylabel('Frequency')
+plt.show()
+
+# 2. Correlation Heatmap
+plt.figure(figsize=(10, 8))
+numeric_cols = df.select_dtypes(include=[np.number])
+sns.heatmap(numeric_cols.corr(), annot=True, cmap='coolwarm', fmt=".2f")
+plt.title('Correlation Heatmap')
+plt.show()
